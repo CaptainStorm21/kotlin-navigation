0e8591557968a7ace17d1057898697b53a6f9a76
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-22 01:28:26 -0700

    135 nav bottom nav

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -1,29 +1,41 @@
 package com.example.navigation
 
+
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-//navigation import rememberNavController()
-import androidx.navigation.compose.rememberNavController
-import com.example.navigation.ui.theme.NavigationTheme
-import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.navigation.ui.theme.DetailScreen
 import com.example.navigation.ui.theme.FeedScreen
 import com.example.navigation.ui.theme.ListScreen
+import com.example.navigation.ui.theme.NavigationTheme
 import com.example.navigation.ui.theme.ProfileScreen
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 
-//refactoring the nav contorllers
+//refactoring the nav contorellers
 
 sealed class Destination(val route: String){
     object Home: Destination(route = "Home")
@@ -78,14 +90,15 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-                    NavigationAppHost(navController = navController)
+                    //NavigationAppHost(navController = navController)
+                    BottomNavTest(navController = navController)
                 }
             }
         }
     }
 }
 
-@Composable()
+@Composable
 fun NavigationAppHost(navController: NavHostController){
     val ctx = LocalContext.current;
 
@@ -105,3 +118,53 @@ fun NavigationAppHost(navController: NavHostController){
         }
     }
 
+
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavTest(navController: NavHostController){
+    val materialBlue100= Color(0xFFf4c2c2)
+
+
+    Scaffold(bottomBar = {
+        BottomNavigation{
+            val navBackStackEntry = navController.currentBackStackEntry
+            val currentDestination = navBackStackEntry?.destination
+
+            BottomNavigationItem(
+                selected = currentDestination?.route == Destination.Home.route,
+                onClick = { navController.navigate(Destination.Home.route) },
+                icon = { Icon(Icons.Default.Home, contentDescription = null)},
+                label = { Text(text = Destination.Home.route)}
+            )
+            BottomNavigationItem(
+                selected = currentDestination?.route == Destination.Feed.route,
+                onClick = { navController.navigate(Destination.Feed.route) },
+                icon = { Icon(Icons.Default.List, contentDescription = null)},
+                label = { Text(text = Destination.Feed.route)}
+            )
+            BottomNavigationItem(
+                selected = currentDestination?.route == Destination.List.route,
+                onClick = { navController.navigate(Destination.List.route) },
+                icon = { Icon(Icons.Default.List, contentDescription = null)},
+                label = { Text(text = Destination.List.route)}
+            )
+            BottomNavigationItem(
+                selected = currentDestination?.route == Destination.Profile.route,
+                onClick = { navController.navigate(Destination.Profile.route) },
+                icon = { Icon(Icons.Default.Person, contentDescription = null)},
+                label = { Text(text = Destination.Profile.route)}
+            )
+        }
+    }) {
+        NavHost(navController = navController, startDestination = Destination.Home.route){
+            composable(Destination.Home.route){ HomeScreen(navController) }
+            composable(Destination.Feed.route){ FeedScreen(navController) }
+            composable(Destination.Profile.route){ ProfileScreen(navController) }
+            composable(Destination.List.route){ ListScreen(navController) }
+
+        }
+    }
+}
